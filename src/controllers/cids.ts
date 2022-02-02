769d6f6eb418bb54bb364f56907e04a0e9168f97
xavi <xavings@gmail.com>
2022-02-02 17:07:53 +0100

    project rename

--- src/controllers/cids.ts
+++ src/controllers/cids.ts
@@ -38,34 +38,4 @@ const update = async (req: Request, res: Response, next: NextFunction) => {
   });
 };
 
-/*
-// deleting a post
-const deletePost = async (req: Request, res: Response, next: NextFunction) => {
-  // get the post id from req.params
-  let id: string = req.params.id;
-  // delete the post
-  let response: AxiosResponse = await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`);
-  // return response
-  return res.status(200).json({
-    message: "post deleted successfully",
-  });
-};
-
-// adding a post
-const addPost = async (req: Request, res: Response, next: NextFunction) => {
-  // get the data from req.body
-  let title: string = req.body.title;
-  let body: string = req.body.body;
-  // add the post
-  let response: AxiosResponse = await axios.post(`https://jsonplaceholder.typicode.com/posts`, {
-    title,
-    body,
-  });
-  // return response
-  return res.status(200).json({
-    message: response.data,
-  });
-};
-*/
-//export default { getPosts, getPost, updatePost, deletePost, addPost };
 export default { getCids, restore, update };
--- src/routes/requests.ts
+++ src/routes/requests.ts
@@ -2,11 +2,7 @@ import express from "express";
 import controller from "../controllers/cids";
 const router = express.Router();
 
-//router.get("/posts", controller.getPosts);
 router.get("/iids/:iids", controller.getCids);
 router.put("/restore/:mid", controller.restore);
 router.put("/update/:mid", controller.update);
-//router.delete("/posts/:id", controller.deletePost);
-//router.post("/posts", controller.addPost);
-
 export = router;
